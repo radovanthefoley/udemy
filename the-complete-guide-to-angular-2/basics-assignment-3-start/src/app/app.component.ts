147765879811f5fae10cafe021a2d6db0ec4ea58
Radovan Masaryk <rmasaryk@gmail.com>
2017-10-17 23:37:12 +0200

    ngIf, ngFor, ngStyle, ngClass

--- the-complete-guide-to-angular-2/basics-assignment-3-start/src/app/app.component.ts
+++ the-complete-guide-to-angular-2/basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .white-text {
+        color: white;
+    }
+  `]
 })
 export class AppComponent {
+  paragraphShown = false;
+  clicksHistory: number[] = [];
+
+  decideButtonLabel(): string {
+    if (this.paragraphShown) {
+      return 'Hide Details';
+    } else {
+      return 'Display Details';
+    }
+  }
+
+  onButtonClick(event: Event) {
+    this.paragraphShown = !this.paragraphShown;
+    this.clicksHistory.push(event.timeStamp);
+  }
+
+  decideBackground(entry: number): string {
+    if (this.clicksHistory.indexOf(entry) < 4) {
+      return 'white';
+    } else {
+      return 'blue';
+    }
+  }
 }

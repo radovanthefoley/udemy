f065a964793bd02b6e809e4926a2e9c48e864bb0
Radovan Masaryk <rmasaryk@gmail.com>
2017-11-17 15:34:49 +0100

    course-project: recipe-list component

--- the-complete-guide-to-angular-2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ the-complete-guide-to-angular-2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    {
+      name: 'A tasty test',
+      description: 'This is a tasty test',
+      imagePath: 'http://maxpixel.freegreatpicture.com/static/photo/2x/Recipe-Soup-Noodle-Curried-Spicy-Chicken-Khaosoi-2344152.jpg'
+    },
+    {
+      name: 'A tasty test 2',
+      description: 'This is a tasty test 2',
+      imagePath: 'http://maxpixel.freegreatpicture.com/static/photo/2x/Recipe-Soup-Noodle-Curried-Spicy-Chicken-Khaosoi-2344152.jpg'
+    },
+    {
+      name: 'A tasty test 3',
+      description: 'This is a tasty test 3',
+      imagePath: 'http://maxpixel.freegreatpicture.com/static/photo/2x/Recipe-Soup-Noodle-Curried-Spicy-Chicken-Khaosoi-2344152.jpg'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
